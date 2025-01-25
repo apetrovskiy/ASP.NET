d4be61d75c081a8a4654197df7f0f81e827a6380
Alexander Petrovskiy <uiautomation@hotmail.com>
2025-01-25 21:33:01 +0300

    experimental changes to connect to the service from the same execution thread

--- Homeworks/Base/run.sh
+++ Homeworks/Base/run.sh
@@ -7,4 +7,4 @@ dotnet format -v d "${SOLUTION_FILE}"
 
 dotnet clean "${SOLUTION_FILE}" && dotnet build "${SOLUTION_FILE}"
 
-dotnet run --project "${MAIN_PROJECT}"
+dotnet run --project "${MAIN_PROJECT}" &
