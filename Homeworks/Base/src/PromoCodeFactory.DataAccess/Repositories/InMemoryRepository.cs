4a3221a460ef85a43fd5209293134bb68b7eb6b6
Alexander Petrovskiy <uiautomation@hotmail.com>
2025-01-28 03:05:29 +0300

    deletion is done && prepared to update

--- Homeworks/Base/src/PromoCodeFactory.DataAccess/Repositories/InMemoryRepository.cs
+++ Homeworks/Base/src/PromoCodeFactory.DataAccess/Repositories/InMemoryRepository.cs
@@ -24,64 +24,53 @@ namespace PromoCodeFactory.DataAccess.Repositories
 
         public Task<IEnumerable<T>> GetAllAsync()
         {
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"GetAllAsync: {d.Id}"));
-            //
             return Task.FromResult(Data);
         }
 
         public Task<T> GetByIdAsync(Guid id)
         {
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"GetByIdAsync: {d.Id}"));
-            //
             return Task.FromResult(Data.FirstOrDefault(x => x.Id == id));
         }
         public Task<IEnumerable<T>> GetRangeByIdsAsync(List<Guid> ids)
         {
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"GetRangeByIdsAsync: {d.Id}"));
-            //
             return Task.FromResult(Data.Where(x => ids.Contains(x.Id)).AsEnumerable());
         }
         public async Task AddAsync(T entity)
         {
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"AddAsync 1: {d.Id}"));
-            //
             Data = await Task.FromResult(Data.Append(entity));
-            // Data = Data.Append(entity);
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"AddAsync 2: {d.Id}"));
-            //
             return;
         }
         public async Task UpdateAsync(T entity)
         {
-            var item = Data.FirstOrDefault(x => x.Id == entity.Id);
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"UpdateAsync 1: {d.Id}"));
-            //
-            await Task.FromResult(Data.ToList().Remove(item));
-            Data = await Task.FromResult(Data.Append(entity));
-            // Data.ToList().Remove(item);
-            // Data = Data.Append(entity);
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"UpdateAsync 2: {d.Id}"));
-            //
+            await Task.FromResult(() =>
+            {
+                // var item = Data.FirstOrDefault(x => x.Id == entity.Id);
+                //
+                // Data.ToList().ForEach(d => Console.WriteLine($"UpdateAsync 1: {d.Id}"));
+                //
+                var preliminaryCollecition = Data.ToList();
+                var item = preliminaryCollecition.FirstOrDefault(x => x.Id == entity.Id);
+                preliminaryCollecition.Remove(item);
+                Data = preliminaryCollecition;
+                Data = Data.Append(entity);
+                // Data.ToList().Remove(item);
+                // Data = Data.Append(entity);
+                //
+                // Data.ToList().ForEach(d => Console.WriteLine($"UpdateAsync 2: {d.Id}"));
+                //
+            });
             return;
         }
         public async Task DeleteAsync(T entity)
         {
-            var item = Data.FirstOrDefault(x => x.Id == entity.Id);
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"DeleteAsync 1: {d.Id}"));
-            //
-            await Task.FromResult(Data.ToList().Remove(item));
-            //  Data.ToList().Remove(item);
-            //
-            // Data.ToList().ForEach(d => Console.WriteLine($"DeleteAsync 2: {d.Id}"));
-            //
+            await Task.FromResult(() =>
+            {
+                // var item = Data.FirstOrDefault(x => x.Id == entity.Id);
+                var preliminaryCollecition = Data.ToList();
+                var item = preliminaryCollecition.FirstOrDefault(x => x.Id == entity.Id);
+                preliminaryCollecition.Remove(item);
+                Data = preliminaryCollecition;
+            });
             return;
         }
     }
--- Homeworks/Base/src/PromoCodeFactory.WebHost/Controllers/EmployeesController.cs
+++ Homeworks/Base/src/PromoCodeFactory.WebHost/Controllers/EmployeesController.cs
@@ -59,14 +59,12 @@ namespace PromoCodeFactory.WebHost.Controllers
             Console.WriteLine($"GetEmployeeByIdAsync guid 0001 {id}");
             //
             var employee = await _employeeRepository.GetByIdAsync(id);
-            //
-            Console.WriteLine($"GetEmployeeByIdAsync guid 0002 {employee.Id}, {employee.FirstName}, {employee.LastName}, {employee.Email}");
-            //
+
 
             if (employee == null)
                 return NotFound();
             //
-            Console.WriteLine($"GetEmployeeByIdAsync guid 0003 +");
+            Console.WriteLine($"GetEmployeeByIdAsync guid 0002 {employee.Id}, {employee.FirstName}, {employee.LastName}, {employee.Email}");
             //
 
             var employeeModel = new EmployeeResponse()
@@ -90,94 +88,8 @@ namespace PromoCodeFactory.WebHost.Controllers
             return employeeModel;
         }
 
-        // /// <summary>
-        // /// Получить данные сотрудника по Id
-        // /// </summary>
-        // /// <returns></returns>
-        // [HttpGet("{id}")]
-        // public async Task<ActionResult<EmployeeResponse>> GetEmployeeByIdAsync([FromRoute] string id)
-        // {
-        //     //
-        //     Console.WriteLine($"GetEmployeeByIdAsync string 0001 {id}");
-        //     //
-        //     var employee = await _employeeRepository.GetByIdAsync(Guid.Parse(id));
-        //     //
-        //     Console.WriteLine($"GetEmployeeByIdAsync string 0002 {employee.Id}, {employee.FirstName}, {employee.LastName}, {employee.Email}");
-        //     //
 
-        //     if (employee == null)
-        //         return NotFound();
-        //     //
-        //     Console.WriteLine($"GetEmployeeByIdAsync string 0003 +");
-        //     //
 
-        //     var employeeModel = new EmployeeResponse()
-        //     {
-        //         Id = employee.Id,
-        //         Email = employee.Email,
-        //         Roles = employee.Roles.Select(x => new RoleItemResponse()
-        //         {
-        //             // why is this hidden?
-        //             Id = x.Id,
-        //             Name = x.Name,
-        //             Description = x.Description
-        //         }).ToList(),
-        //         FullName = employee.FullName,
-        //         AppliedPromocodesCount = employee.AppliedPromocodesCount
-        //     };
-        //     //
-        //     Console.WriteLine($"GetEmployeeByIdAsync string 0004 {employeeModel.Id}, {employeeModel.FullName}, {employeeModel.Email}");
-        //     //
-
-        //     return employeeModel;
-        // }
-
-        // public class Dto
-        // {
-        //     public Guid Id { get; set; }
-        // }
-
-        // /// <summary>
-        // /// Получить данные сотрудника по Id
-        // /// </summary>
-        // /// <returns></returns>
-        // [HttpGet]
-        // public async Task<ActionResult<EmployeeResponse>> GetEmployeeByIdAsync([FromBody] Dto dto)
-        // {
-        //     //
-        //     Console.WriteLine($"GetEmployeeByIdAsync dto 0001 {dto}");
-        //     //
-        //     var employee = await _employeeRepository.GetByIdAsync(dto.Id);
-        //     //
-        //     Console.WriteLine($"GetEmployeeByIdAsync dto 0002 {employee.Id}, {employee.FirstName}, {employee.LastName}, {employee.Email}");
-        //     //
-
-        //     if (employee == null)
-        //         return NotFound();
-        //     //
-        //     Console.WriteLine($"GetEmployeeByIdAsync dto 0003 +");
-        //     //
-
-        //     var employeeModel = new EmployeeResponse()
-        //     {
-        //         Id = employee.Id,
-        //         Email = employee.Email,
-        //         Roles = employee.Roles.Select(x => new RoleItemResponse()
-        //         {
-        //             // why is this hidden?
-        //             Id = x.Id,
-        //             Name = x.Name,
-        //             Description = x.Description
-        //         }).ToList(),
-        //         FullName = employee.FullName,
-        //         AppliedPromocodesCount = employee.AppliedPromocodesCount
-        //     };
-        //     //
-        //     Console.WriteLine($"GetEmployeeByIdAsync dto 0004 {employeeModel.Id}, {employeeModel.FullName}, {employeeModel.Email}");
-        //     //
-
-        //     return employeeModel;
-        // }
 
         /// <summary>
         /// Создать нового сотрудника
@@ -197,41 +109,33 @@ namespace PromoCodeFactory.WebHost.Controllers
             // return CreatedAtAction(nameof(GetCustomerAsync), new { id = customer.Id }, customer.Id);
 
             Console.WriteLine($"POST input {request.FirstName}, {request.LastName}, {request.Email}");
-            // , {request.Roles[0].Id}");
-            // try
-            // {
-            // TOOD:        var roles = await _roleRepository.GetRangeByIdsAsync(request.Roles.Select(x => x.Id).ToList());
-            // TODO: mapper
             // Employee employee = new() { Id = Guid.Parse("451533d5-d8d5-4a11-9c7b-eb9f14e1a32f"), FirstName = request.FirstName, LastName = request.LastName, Email = request.Email, Roles = roles.ToList() };
             var employee = EmployeeMapper.MapFromModel(request, []); // TODO: , roles);
             Console.WriteLine($"id={employee.Id}, first={employee.FirstName}, last={employee.LastName}, email={employee.Email}");
             //, Roles = request.Roles.Select(role => new Role() { Name = role.Name, Description = role.Description }) };
-            // Console.WriteLine("CreateEmployeeAsync 0006");
             await _employeeRepository.AddAsync(employee);
-            // Console.WriteLine("CreateEmployeeAsync 0007");
-            // Console.WriteLine($"nameof(GetEmployeeByIdAsync) = {nameof(GetEmployeeByIdAsync)}");
-            // Console.WriteLine($"employee.Id = {employee.Id}");
-            Console.WriteLine($"new id = employee.Id = {new { id = employee.Id }}");
-            //
-            try
-            {
-                Console.WriteLine($"GetByIdAsync(employee.Id); {employee.Id}");
-                (_employeeRepository as InMemoryRepository<Employee>).Data.ToList().ForEach(d => Console.WriteLine($"data item: {d.Id}, {d.Email}"));
-                var saved = await _employeeRepository.GetByIdAsync(employee.Id);
-                Console.WriteLine($"before?");
-                Console.WriteLine($"null == saved {null == saved}");
-                if (null != saved)
-                {
-                    Console.WriteLine($"saved.Id {saved.Id}");
-                    Console.WriteLine($"saved.FullName {saved.FullName}");
-                    Console.WriteLine($"saved.Email {saved.Email}");
-                    Console.WriteLine($"CreateEmployeeAsync -> GetEmployeeByIdAsync: {saved.Id}, {saved.FullName}, {saved.Email}");
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"{e.Message}; {e.StackTrace}");
-            }
+            // //
+            // Console.WriteLine($"new id = employee.Id = {new { id = employee.Id }}");
+            // //
+            // try
+            // {
+            //     Console.WriteLine($"GetByIdAsync(employee.Id); {employee.Id}");
+            //     (_employeeRepository as InMemoryRepository<Employee>).Data.ToList().ForEach(d => Console.WriteLine($"data item: {d.Id}, {d.Email}"));
+            //     var saved = await _employeeRepository.GetByIdAsync(employee.Id);
+            //     Console.WriteLine($"before?");
+            //     Console.WriteLine($"null == saved {null == saved}");
+            //     if (null != saved)
+            //     {
+            //         Console.WriteLine($"saved.Id {saved.Id}");
+            //         Console.WriteLine($"saved.FullName {saved.FullName}");
+            //         Console.WriteLine($"saved.Email {saved.Email}");
+            //         Console.WriteLine($"CreateEmployeeAsync -> GetEmployeeByIdAsync: {saved.Id}, {saved.FullName}, {saved.Email}");
+            //     }
+            // }
+            // catch (Exception e)
+            // {
+            //     Console.WriteLine($"{e.Message}; {e.StackTrace}");
+            // }
             var result1 = CreatedAtAction(nameof(GetEmployeeByIdAsync), nameof(EmployeesController), new { Id = employee.Id.ToString() }, employee.Id.ToString());
             Console.WriteLine($"{result1.ActionName}, {result1.ControllerName}, {result1.RouteValues}, {result1.Value}");
             var result2 = CreatedAtAction("GetEmployeeById", new { id = employee.Id }, employee.Id);
@@ -246,17 +150,6 @@ namespace PromoCodeFactory.WebHost.Controllers
             // return CreatedAtAction("GetEmployeeById", new { id = employee.Id.ToString() }, employee.Id.ToString());
             // TODO: is working
             return CreatedAtAction("GetEmployeeById", new { id = employee.Id }, employee.Id);
-            // return CreatedAtAction(nameof(GetEmployeeByIdAsync), new { id = employee.Id }, employee.Id);
-            // return CreatedAtAction(nameof(GetEmployeeByIdAsync), new { Id = employee.Id }, employee.Id);
-            // return CreatedAtAction(nameof(GetEmployeeByIdAsync), nameof(EmployeesController), new { Id = employee.Id.ToString() }, employee.Id.ToString());
-            // return CreatedAtAction(nameof(GetEmployeeByIdAsync), employee.Id);
-            // return CreatedAtAction(nameof(GetEmployeeByIdAsync), employee.Id, employee.Id);
-            // }
-            // catch (Exception e)
-            // {
-            //     Console.WriteLine($"{e} {e.Message} {e.StackTrace}");
-            //     return NotFound();
-            // }
         }
 
         /// <summary>
@@ -266,16 +159,6 @@ namespace PromoCodeFactory.WebHost.Controllers
         [HttpDelete("{id:guid}")]
         public async Task<IActionResult> DeleteEmployeeByIdAsync(Guid id)
         {
-            /*
-            var customer = await _customerRepository.GetByIdAsync(id);
-
-            if (customer == null)
-                return NotFound();
-
-            await _customerRepository.DeleteAsync(customer);
-
-            return NoContent();
-            */
             var employee = await _employeeRepository.GetByIdAsync(id);
             if (null == employee) return NotFound();
             await _employeeRepository.DeleteAsync(employee);
